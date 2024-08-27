61a284f2a9e1bdc850615afb27c9c9abc5f194b4
Willy Micieli <otechdo@otechdo.com>
2024-08-27 10:06:55 +0200

    clea code

--- src/clippy/mod.rs
+++ src/clippy/mod.rs
@@ -1,14 +1,68 @@
-#[doc = "absolute_path lints"]
-pub mod absolute_path;
+use crate::config::{Applicability, ClippyGroup, Lint, Severity};
 
-#[doc = "assigning_clones lints"]
-pub mod assigning_clones;
+#[doc = "The absolute path lints for all"]
+pub const ABSOLUTE_PATH_LINTS: (Lint, Lint, Lint) = Lint::new(
+    "absolute_paths",
+    "Checks for usage of items through absolute paths, like std::env::current_dir.",
+    &Severity::Allow,
+    &Severity::Warn,
+    &Severity::Deny,
+    ClippyGroup::Restriction,
+    Applicability::Unspecified,
+);
 
-#[doc = "blocks_in_conditions lints"]
-pub mod blocks_in_conditions;
+#[doc = "The allow attribute without reason lints for all"]
+pub const ALLOW_ATTRIBUTES_WITHOUT_REASON_LINTS: (Lint, Lint, Lint) = Lint::new(
+    "allow_attributes_without_reason",
+    "Checks for attributes that allow lints without a reason.",
+    &Severity::Warn,
+    &Severity::Warn,
+    &Severity::Deny,
+    ClippyGroup::Restriction,
+    Applicability::Unspecified,
+);
 
-#[doc = "allow_attributes_without_reason lints"]
-pub mod allow_attributes_without_reason;
+#[doc = "The almost complete range lints for all"]
+pub const ALMOST_COMPLETE_RANGE_LINTS: (Lint, Lint, Lint) = Lint::new(
+    "almost_complete_range",
+    "Checks for ranges which almost include the entire range of letters or digits.",
+    &Severity::Warn,
+    &Severity::Warn,
+    &Severity::Deny,
+    ClippyGroup::Restriction,
+    Applicability::Unspecified,
+);
 
-#[doc = "almost_complete_range lints"]
-pub mod almost_complete_range;
+#[doc = "The also swaped lints for all"]
+pub const ALMOST_SWAPED_LINTS: (Lint, Lint, Lint) = Lint::new(
+    "almost_swaped",
+    "Checks for foo = bar; bar = foo sequences.",
+    &Severity::Deny,
+    &Severity::Deny,
+    &Severity::Deny,
+    ClippyGroup::Style,
+    Applicability::MachineApplicable,
+);
+
+#[doc = "The assigning clone lints for all"]
+pub const ASSIGNING_CLONES_LINTS: (Lint, Lint, Lint) = Lint::new(
+    "assigning_clones",
+    "Checks for code like foo = bar.clone();",
+    &Severity::Warn,
+    &Severity::Warn,
+    &Severity::Deny,
+    ClippyGroup::Pedantic,
+    Applicability::Unspecified,
+);
+
+#[doc = "The block in condition lints for all"]
+pub const BLOCKS_IN_CONDITIONS_LINTS: (Lint, Lint, Lint) = Lint::new(
+    "blocks_in_conditions",
+    "
+Checks for if and match conditions that use blocks containing an expression, statements or conditions that use closures with blocks.",
+    &Severity::Warn,
+    &Severity::Warn,
+    &Severity::Deny,
+    ClippyGroup::Style,
+    Applicability::MachineApplicable,
+);
--- src/lints/mod.rs
+++ src/lints/mod.rs
@@ -1,20 +1,19 @@
 use crate::{
     clippy::{
-        absolute_path::ABSOLUTE_PATH_LINTS,
-        allow_attributes_without_reason::ALLOW_ATTRIBUTES_WITHOUT_REASON_LINTS,
-        almost_complete_range::ALMOST_COMPLETE_RANGE_LINTS,
-        assigning_clones::ASSIGNING_CLONES_LINTS, blocks_in_conditions::BLOCKS_IN_CONDITIONS_LINTS,
+        ABSOLUTE_PATH_LINTS, ALLOW_ATTRIBUTES_WITHOUT_REASON_LINTS, ALMOST_COMPLETE_RANGE_LINTS,
+        ALMOST_SWAPED_LINTS, ASSIGNING_CLONES_LINTS, BLOCKS_IN_CONDITIONS_LINTS,
     },
     config::Lint,
 };
 
-const LINTERS: usize = 5;
+const LINTERS: usize = 6;
 
 const ABSOLUTE_PATH: (Lint, Lint, Lint) = ABSOLUTE_PATH_LINTS;
 const ASSIGNING_CLONES: (Lint, Lint, Lint) = ASSIGNING_CLONES_LINTS;
 const BLOCKS_IN_CONDITIONS: (Lint, Lint, Lint) = BLOCKS_IN_CONDITIONS_LINTS;
 const ALLOW_ATTRIBUTES_WITHOUT_REASON: (Lint, Lint, Lint) = ALLOW_ATTRIBUTES_WITHOUT_REASON_LINTS;
 const ALMOST_COMPLETE_RANGE_LINT: (Lint, Lint, Lint) = ALMOST_COMPLETE_RANGE_LINTS;
+const ALMOST_SWAPED_LINT: (Lint, Lint, Lint) = ALMOST_SWAPED_LINTS;
 
 #[doc = "All lints for novice"]
 pub const NOVICE_LINTS: [Lint; LINTERS] = [
@@ -23,6 +22,7 @@ pub const NOVICE_LINTS: [Lint; LINTERS] = [
     BLOCKS_IN_CONDITIONS.0,
     ALLOW_ATTRIBUTES_WITHOUT_REASON.0,
     ALMOST_COMPLETE_RANGE_LINT.0,
+    ALMOST_SWAPED_LINT.0,
 ];
 
 #[doc = "All lints for expert"]
@@ -32,6 +32,7 @@ pub const EXPERT_LINTS: [Lint; LINTERS] = [
     BLOCKS_IN_CONDITIONS.1,
     ALLOW_ATTRIBUTES_WITHOUT_REASON.1,
     ALMOST_COMPLETE_RANGE_LINT.1,
+    ALMOST_SWAPED_LINT.1,
 ];
 
 #[doc = "All lints for master"]
@@ -41,4 +42,5 @@ pub const MASTER_LINTS: [Lint; LINTERS] = [
     BLOCKS_IN_CONDITIONS.2,
     ALLOW_ATTRIBUTES_WITHOUT_REASON.2,
     ALMOST_COMPLETE_RANGE_LINT.2,
+    ALMOST_SWAPED_LINT.2,
 ];
