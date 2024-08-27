64ba62d6326cd12286c094deb143df0b28d82374
Willy Micieli <otechdo@otechdo.com>
2024-08-27 08:44:41 +0200

    add allow_attributes_without_reason

--- src/clippy/mod.rs
+++ src/clippy/mod.rs
@@ -6,3 +6,6 @@ pub mod assigning_clones;
 
 #[doc = "blocks_in_conditions lints"]
 pub mod blocks_in_conditions;
+
+#[doc = "allow_attributes_without_reason lints"]
+pub mod allow_attributes_without_reason;
--- src/lints/mod.rs
+++ src/lints/mod.rs
@@ -1,25 +1,39 @@
 use crate::{
     clippy::{
-        absolute_path::ABSOLUTE_PATH_LINTS, assigning_clones::ASSIGNING_CLONES_LINTS,
-        blocks_in_conditions::BLOCKS_IN_CONDITIONS_LINTS,
+        absolute_path::ABSOLUTE_PATH_LINTS,
+        allow_attributes_without_reason::ALLOW_ATTRIBUTES_WITHOUT_REASON_LINTS,
+        assigning_clones::ASSIGNING_CLONES_LINTS, blocks_in_conditions::BLOCKS_IN_CONDITIONS_LINTS,
     },
     config::Lint,
 };
 
-const LINTERS: usize = 3;
+const LINTERS: usize = 4;
 
 const ABSOLUTE_PATH: (Lint, Lint, Lint) = ABSOLUTE_PATH_LINTS;
 const ASSIGNING_CLONES: (Lint, Lint, Lint) = ASSIGNING_CLONES_LINTS;
 const BLOCKS_IN_CONDITIONS: (Lint, Lint, Lint) = BLOCKS_IN_CONDITIONS_LINTS;
+const ALLOW_ATTRIBUTES_WITHOUT_REASON: (Lint, Lint, Lint) = ALLOW_ATTRIBUTES_WITHOUT_REASON_LINTS;
 
 #[doc = "All lints for novice"]
-pub const NOVICE_LINTS: [Lint; LINTERS] =
-    [ABSOLUTE_PATH.0, ASSIGNING_CLONES.0, BLOCKS_IN_CONDITIONS.0];
+pub const NOVICE_LINTS: [Lint; LINTERS] = [
+    ABSOLUTE_PATH.0,
+    ASSIGNING_CLONES.0,
+    BLOCKS_IN_CONDITIONS.0,
+    ALLOW_ATTRIBUTES_WITHOUT_REASON.0,
+];
 
 #[doc = "All lints for expert"]
-pub const EXPERT_LINTS: [Lint; LINTERS] =
-    [ABSOLUTE_PATH.1, ASSIGNING_CLONES.1, BLOCKS_IN_CONDITIONS.1];
+pub const EXPERT_LINTS: [Lint; LINTERS] = [
+    ABSOLUTE_PATH.1,
+    ASSIGNING_CLONES.1,
+    BLOCKS_IN_CONDITIONS.1,
+    ALLOW_ATTRIBUTES_WITHOUT_REASON.1,
+];
 
 #[doc = "All lints for master"]
-pub const MASTER_LINTS: [Lint; LINTERS] =
-    [ABSOLUTE_PATH.2, ASSIGNING_CLONES.2, BLOCKS_IN_CONDITIONS.2];
+pub const MASTER_LINTS: [Lint; LINTERS] = [
+    ABSOLUTE_PATH.2,
+    ASSIGNING_CLONES.2,
+    BLOCKS_IN_CONDITIONS.2,
+    ALLOW_ATTRIBUTES_WITHOUT_REASON.2,
+];
